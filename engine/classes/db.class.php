2f711241ec5a4ecde1d255a6c7282d770b984a26
dmitriy <dsmurygin@gmail.com>
2015-08-11 12:12:33 +0600

    1

--- engine/classes/db.class.php
+++ engine/classes/db.class.php
@@ -1,24 +1,20 @@
 <?php
 class db{
     private static $link;
+    public $className = 'stdClass';
 
     function __construct(){
         self::$link = new PDO('mysql:host=localhost;dbname=audiobooks;charset=utf8', 'web', 'Noname1990', array(
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_CLASS,
             PDO::ATTR_PERSISTENT         => true
         ));
     }
 
-    public static function query($sql, $params = null) {
-        if (is_null($params)){
-            $query = self::$link->query($sql);
-        }
-        else{
-            $query = self::$link->prepare($sql);
-            $query->execute($params);
-        }
+    public function query($sql, $params = []) {
+        $query = self::$link->prepare($sql);
+        $query->execute($params);
 
-        return $query->fetchAll();
+        return $query->fetchAll(PDO::FETCH_CLASS, $this->className);
     }
 }
