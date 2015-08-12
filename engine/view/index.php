d67ec676ae7d4962f5a05d1f512867cf60f01937
dsmurygin <dsmurygin@gmail.com>
2015-08-12 09:26:13 +0600

    1

--- engine/view/index.php
+++ engine/view/index.php
@@ -42,7 +42,7 @@
                 <h3>Жанры</h3>
                 <ul class="side_menu">
                     <?php foreach ($categories as $cat): ?>
-                        <li><a onclick="showContent("category","<?php echo $cat->category_url ?>","");return false" href="/category/<?php echo $cat->category_url ?>"><?php echo $cat->category_name ?></a></li>
+                        <li><a onclick="showContent('category','<?php echo $cat->category_url ?>','');return false" href="/category/<?php echo $cat->category_url ?>"><?php echo $cat->category_name ?></a></li>
                     <?php endforeach?>
                 </ul>
             </div>
@@ -52,16 +52,19 @@
                 <h3>Топ 20 аудиокниг</h3>
                 <!--noindex-->
                 <ul class="side_menu">
-                    <?php
-                    $query=$pdo->query('SELECT book_name, book_url, book_votes, book_id FROM books ORDER by book_votes DESC LIMIT 0,20');
-                    $i=1;
-                    while ($row=$query->fetch()){
-                        $book = new book();
-                        $book->id = $row['book_id'];
-                        echo '<li>'.$i.'. <a rel="nofollow" href="/book/'.$row['book_url'].'" onclick="showContent(\'book\',\''.$row['book_url'].'\',\'\');return false">'.$row['book_name'].' - '.$book->getAuthors()['authors'].'</a> ('.$row['book_votes'].')</li>';
-                        $i++;
-                    }
-                    ?>
+                    <?php $i = 1; foreach ($top20 as $top): ?>
+                        <li><?php echo $i ?>. <a rel="nofollow" href="/book/<?php echo $top->book_url ?>" onclick="showContent('book','<?php echo $top->book_url ?>','');return false"><?php echo $top->book_name ?> -
+                            <?php
+                            $i = 0;
+                            foreach ($top->authors as $author):
+                                echo $author->author_name;
+                                $i++;
+                                if ($i !== count($author)){
+                                    echo ', ';
+                                }
+                            endforeach ?>
+                            </a> (<?php echo $top->book_votes ?>)</li>
+                    <?php $i++; endforeach ?>
                 </ul>
                 <!--/noindex-->
             </div>
@@ -89,10 +92,10 @@
 <script src="/js/script.js?130"></script>
 <script src="/js/player.js?125"></script>
 <?php
-echo '<script>';
+/*echo '<script>';
 if ($content->eof) echo 'eof = true;'; else echo 'eof = false;';
 if ($src!='') echo ' idBook = '.$bookId.'; bookURL = "'.$_COOKIE['bookURL'].'"; $time = '.$time.';';
-echo '</script>';
+echo '</script>';*/
 ?>
 <script> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter29349035 = new Ya.Metrika({ id:29349035, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/29349035" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 </body>
