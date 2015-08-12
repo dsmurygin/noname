6262a3f32bff245594541e7dec324741ce81dac4
dsmurygin <dsmurygin@gmail.com>
2015-08-12 09:25:45 +0600

    fix function gettobook

--- engine/classes/authors.class.php
+++ engine/classes/authors.class.php
@@ -4,7 +4,7 @@ class authors
 {
     public function getForBook($book_id){
         $db = new db;
-        $this->authors = $db->query('SElECT a.author_name,a.author_url FROM books b, authors a, books_authors ba
+        return $db->query('SElECT a.author_name,a.author_url FROM books b, authors a, books_authors ba
                                      WHERE ba.book_id = b.book_id AND ba.author_id = a.author_id AND b.book_id =' . $book_id);
     }
 }
