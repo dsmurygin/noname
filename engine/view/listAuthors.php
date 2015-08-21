113c391c9827978ce1f3379109e619d742dedeaf
dsmurygin <dsmurygin@gmail.com>
2015-08-21 20:06:59 +0600

    file created, list all authors

---
+++ engine/view/listAuthors.php
@@ -0,0 +1,5 @@
+<?php
+
+foreach ($data->content as $author):?>
+    <a href="/author/<?= $author->author_url ?>" onclick="showContent('author','<?= $author->author_url ?>','');return false"><?= $author->author_name ?></a><br>
+<?php endforeach ?>
