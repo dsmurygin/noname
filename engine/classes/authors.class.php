dfbcf4d2350119dcbf1a479f8c48346e475f154a
dmitriy <dsmurygin@gmail.com>
2015-08-21 10:26:00 +0600

    complete this class

--- engine/classes/authors.class.php
+++ engine/classes/authors.class.php
@@ -8,7 +8,7 @@ class authors
     public function getForBook($book_id){
         $db = new db;
         return $db->query('SElECT a.author_name,a.author_url FROM books b, authors a, books_authors ba
-                                     WHERE ba.book_id = b.book_id AND ba.author_id = a.author_id AND b.book_id =' . $book_id);
+                                     WHERE ba.book_id = b.book_id AND ba.author_id = a.author_id AND b.book_id = ' . $book_id);
     }
 
     public function getlistAuthors($book_id){
