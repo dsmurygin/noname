b95cbe28f9b755a4072c30ddd1d58eac8769b69c
dmitriy <dsmurygin@gmail.com>
2015-08-21 12:30:40 +0600

    this script finished

--- engine/view/index.php
+++ engine/view/index.php
@@ -99,8 +99,8 @@
 <script src = "/js/player.js?125"></script>
 <?php
 echo '<script>';
-if ($content->eof) echo 'eof = true;'; else echo 'eof = false;';
-if ($src!='') echo ' idBook = '.$bookId.'; bookURL = "'.$_COOKIE['bookURL'].'"; $time = '.$time.';';
+if ($content['eof']) echo 'eof = true;'; else echo 'eof = false;';
+if ($src!='') echo ' idBook = '.$bookId.'; bookURL = "' . $src . '"; $time = '.$time.';';
 echo '</script>';
 ?>
 <script> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter29349035 = new Ya.Metrika({ id:29349035, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/29349035" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
